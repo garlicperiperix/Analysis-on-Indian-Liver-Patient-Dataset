cf86652647755e79e850a79626a3c6c2b38ebd54
Sai Srivatsava Manchala <47390007+garlicperiperix@users.noreply.github.com>
2022-07-07 11:36:09 -0500

    Add files via upload

---
+++ DATABASE.sql
@@ -0,0 +1,14 @@
+use record;
+
+create table record_table(
+Age int,
+Gender int,
+Total_Bilirubin float,
+Direct_Bilirubin float,
+Alkaline_Phosphotase int,
+Alamine_Aminotransferase int,
+Aspartate_Aminotransferase int,
+Total_Protiens float,
+Albumin float,
+Albumin_and_Globulin_Ratio float,
+Dataset int)
